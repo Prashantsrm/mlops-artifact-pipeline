5d11cf77d960f4936cf5fc5c82dd5fbcb7f23242
PrashantMishra <root@DESKTOP-8FNVABR.localdomain>
2025-07-19 20:32:23 +0000

    Add training script with load_config and train_model

--- src/train.py
+++ src/train.py
@@ -1,4 +1,4 @@
-import json
+"""import json
 import pickle
 from sklearn.datasets import load_digits
 from sklearn.linear_model import LogisticRegression
@@ -21,5 +21,25 @@ model.fit(X, y)
 
 # Save trained model
 with open('model_train.pkl', 'wb') as f:
-    pickle.dump(model, f)
+    pickle.dump(model, f)"""
+
+# src/train.py
+
+import json
+from sklearn.linear_model import LogisticRegression
+
+def load_config(config_path='config/config.json'):
+    with open(config_path, 'r') as f:
+        config = json.load(f)
+    return config
+
+def train_model(X, y, config):
+    model = LogisticRegression(
+        C=config["C"],
+        solver=config["solver"],
+        max_iter=config["max_iter"]
+    )
+    model.fit(X, y)
+    return model
+
 
